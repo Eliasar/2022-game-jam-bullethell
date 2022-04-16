e2ccfab3dcfe38f4f6311ab0587cdeb4a0449489
Mike W <khsdrummer2001@gmail.com>
2022-04-16 10:00:16 -0500

    Moved one folder level deeper

---
+++ Assets/Scripts/TestBulletSpawn.cs
@@ -0,0 +1,28 @@
+using BulletFury;
+using BulletFury.Data;
+using UnityEngine;
+
+namespace Confined
+{
+    public class TestBulletSpawn : MonoBehaviour
+    {
+        [SerializeField] private BulletManager bulletManager = null;
+        [SerializeField] private float rotateSpeed = 0f;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = 120;
+        }
+
+        private void Update()
+        {
+            if (bulletManager == null)
+            {
+                return;
+            }
+
+            bulletManager.Spawn(transform.position, bulletManager.Plane == BulletPlane.XY ? transform.up : transform.forward);
+            transform.Rotate(bulletManager.Plane == BulletPlane.XY ? Vector3.forward : Vector3.up, (rotateSpeed * Time.smoothDeltaTime));
+        }
+    }
+}
